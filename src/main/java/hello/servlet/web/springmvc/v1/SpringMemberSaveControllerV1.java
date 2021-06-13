d4b5fd5d7b85ea46aac7e8ac1e10a6f555bacb0c
mjlee <minjun0124@gmail.com>
2021-06-13 19:03:18 +0900

    feat: spring mvc

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,33 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        // ModelView: 전달 경로의 논리이름과 데이터 정보를 저장.
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+        return mv;
+    }
+}
